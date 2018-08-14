72a8d4171a1daec63679c60b8cf893f750084de9
Geoffrey J. Teale <tealeg@gmail.com>
2018-08-14 15:46:54 +0200

    Implement GetAllLeagues

--- competition/db.go
+++ competition/db.go
@@ -1,6 +1,8 @@
-package league
+package competition
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -11,13 +13,17 @@ type DB struct {
 	db *bolt.DB
 }
 
+var (
+	leagueBN []byte = []byte("league")
+)
+
 func NewDB(name string) (*DB, error) {
 	db, err := bolt.Open(fmt.Sprintf("%s.db", name), 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		return nil, err
 	}
 	err = db.Update(func(tx *bolt.Tx) error {
-		if _, err := tx.CreateBucketIfNotExists([]byte("league")); err != nil {
+		if _, err := tx.CreateBucketIfNotExists(leagueBN); err != nil {
 			return err
 		}
 
@@ -26,7 +32,32 @@ func NewDB(name string) (*DB, error) {
 	return &DB{db: db}, nil
 }
 
-//
+func (db *DB) GetAllLeagues() ([]League, error) {
+	leagues := make([]League, 0)
+	err := db.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(leagueBN)
+		err := b.ForEach(func(k, v []byte) error {
+			l := League{}
+			err := json.Unmarshal(v, &l)
+			if err != nil {
+				return err
+			}
+			leagues = append(leagues, l)
+			return nil
+		})
+		return err
+
+	})
+	return leagues, err
+}
+
 func (db *DB) Close() error {
 	return db.db.Close()
+}
+
+// itob returns an 8-byte big endian representation of v.
+func itob(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
 }
--- competition/db_test.go
+++ competition/db_test.go
@@ -1,4 +1,4 @@
-package league
+package competition
 
 import (
 	"os"
@@ -22,4 +22,52 @@ func tearDownDB(db *DB) {
 func TestNewDB(t *testing.T) {
 	db := setUpDB(t)
 	defer tearDownDB(db)
+}
+
+func TestCreateAndGetLeague(t *testing.T) {
+	db := setUpDB(t)
+	defer tearDownDB(db)
+
+	l := &League{Name: "English Premier League"}
+
+	id, err := db.CreateLeague(l)
+	if err != nil {
+		t.Fatalf("unexpected error in CreateLeague: %s", err.Error())
+	}
+
+	l2, err := db.GetLeague(id)
+	if err != nil {
+		t.Fatalf("unexpected error GetLeague")
+	}
+	if l2.Name != l.Name {
+		t.Errorf("Expected l2.Name == %q, but got %q", l.Name, l2.Name)
+	}
+
+}
+
+func TestGetAllLeagues(t *testing.T) {
+	db := setUpDB(t)
+	defer tearDownDB(db)
+
+	leagues := []*League{
+		&League{Name: "English Premier League"},
+		&League{Name: "English Championship"},
+		&League{Name: "English FA League 1"},
+		&League{Name: "English FA League 2"},
+	}
+	for i, l := range leagues {
+		_, err := db.CreateLeague(l)
+		if err != nil {
+			t.Fatalf("[%d] Unexpected error in CreateLeague: %s", i, err.Error())
+		}
+	}
+
+	results, err := db.GetAllLeagues()
+	if err != nil {
+		t.Fatalf("unexpected error in GetAllLeagues: %s", err.Error())
+	}
+	if len(results) != 4 {
+		t.Fatalf("expected 4 leagues, got %d", len(results))
+	}
+
 }
