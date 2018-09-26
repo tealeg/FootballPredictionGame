dc4891958a54dfc7c66fafbba922a94124fb724a
Geoffrey J. Teale <tealeg@gmail.com>
2018-09-26 23:27:21 +0200

    Remove old front-page handler

--- app/server.go
+++ app/server.go
@@ -10,7 +10,6 @@ func Serve(port string, adb *user.AccountDB, cdb *competition.DB) {
 	e := echo.New()
 	e.Static("/", "static")
 	setupUserHandlers(e, adb)
-	// setupFrontPageHandler(e, adb, cdb)
 	setupLeagueHandlers(e, adb, cdb)
 	// setupSeasonHandlers(e, adb, cdb)
 	e.Logger.Fatal(e.Start(port))
