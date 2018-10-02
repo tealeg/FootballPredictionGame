944ee1e57f7b5af72c5b4f6754065072d47d64cb
Geoffrey J. Teale <tealeg@gmail.com>
2018-10-03 00:11:08 +0200

    Port db-test.go to testify

--- user/db_test.go
+++ user/db_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	bolt "github.com/coreos/bbolt"
+	"github.com/stretchr/testify/assert"
 )
 
 func setUpAccountDB() (*AccountDB, error) {
@@ -19,140 +20,100 @@ func tearDownAccountDB(adb *AccountDB) {
 
 func TestNewAccountDB(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
-	if err != nil {
-		t.Fatalf("Error creating test DB: %s", err.Error())
-	}
+	assert.NoError(t, err)
 }
 
 func TestCreate(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
 
 	acc := Account{Name: "tealeg"}
 	err = adb.Create(acc)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
+
 	adb.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("account"))
 		key, _ := adb.Key(acc.Name)
 		v := b.Get(key)
 		acc2 := &Account{}
 		err := json.Unmarshal(v, acc2)
-		if err != nil {
-			t.Fatalf("unexpected error unmarshalling account: %s", err.Error())
-			return err
-		}
-		if acc2.Name != acc.Name {
-			t.Errorf("Expected acc2.Name == %q, but got %q", acc.Name, acc2.Name)
-			return nil
-		}
+		assert.NoError(t, err)
+		assert.Equal(t, acc.Name, acc2.Name)
 		return nil
 	})
 }
 
 func TestCreateDuplicate(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
 
 	acc := Account{Name: "tealeg"}
 	err = adb.Create(acc)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
+
 	err = adb.Create(acc)
-	if err == nil {
-		t.Fatalf("expected error in Create, but none occurred")
-	}
+	assert.Error(t, err)
 
 }
 
 func TestGet(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
 
 	acc := Account{Name: "tealeg"}
 	err = adb.Create(acc)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
 	acc2, err := adb.Get("tealeg")
-	if err != nil {
-		t.Fatalf("unexpected in Get: %s", err.Error())
-	}
-	if acc2.Name != acc.Name {
-		t.Errorf("Expected acc2.Name == %q, but got %q", acc.Name, acc2.Name)
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, acc.Name, acc2.Name)
 }
 
 func TestUpdate(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
 
 	acc1 := Account{Name: "tealeg", Forename: "Geoff"}
 	err = adb.Create(acc1)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
 
 	acc2 := Account{Name: "tealeg", Forename: "Gott"}
 	err = adb.Update("tealeg", acc2)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
 
 	acc3, err := adb.Get("tealeg")
-	if err != nil {
-		t.Fatalf("unexpected in Get: %s", err.Error())
-	}
-	if acc3.Forename == acc1.Forename {
-		t.Errorf("Expected acc3.Forename != %q, but got %q", acc1.Forename, acc1.Forename)
-	}
-	if acc3.Forename != acc2.Forename {
-		t.Errorf("Expected acc3.Forename == %q, but got %q", acc2.Forename, acc3.Forename)
-	}
-
+	assert.NoError(t, err)
+	assert.Equal(t, acc3.Forename, acc2.Forename)
+	assert.Equal(t, acc3.Forename, acc2.Forename)
 }
 
 func TestAdminUserExists(t *testing.T) {
 	adb, err := setUpAccountDB()
+	assert.NoError(t, err)
 	defer tearDownAccountDB(adb)
 
 	exists, err := adb.AdminUserExists()
-	if err != nil {
-		t.Fatalf("unexpected error in AdminUserExists: %s", err.Error())
-	}
-	if exists {
-		t.Fatal("expecte AdminUserExists == false, but got true")
-	}
+	assert.NoError(t, err)
+	assert.False(t, exists)
 
 	normalAcc := Account{Name: "nic", IsAdmin: false}
 	err = adb.Create(normalAcc)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
 
 	exists, err = adb.AdminUserExists()
-	if err != nil {
-		t.Fatalf("unexpected error in AdminUserExists: %s", err.Error())
-	}
-	if exists {
-		t.Fatal("expecte AdminUserExists == false, but got true")
-	}
+	assert.NoError(t, err)
+	assert.False(t, exists)
 
 	adminAcc := Account{Name: "geoff", IsAdmin: true}
 	err = adb.Create(adminAcc)
-	if err != nil {
-		t.Fatalf("unexpected error in Create: %s", err.Error())
-	}
+	assert.NoError(t, err)
 
 	exists, err = adb.AdminUserExists()
-	if err != nil {
-		t.Fatalf("unexpected error in AdminUserExists: %s", err.Error())
-	}
-	if !exists {
-		t.Fatal("expecte AdminUserExists == true, but got false")
-	}
-
+	assert.NoError(t, err)
+	assert.True(t, exists)
 }
